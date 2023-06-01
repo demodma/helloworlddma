49acdf338fbb82b61ebe7794fa0f70d69ce4e3eb
fgomes <fgomes@FGOMES-GCF6LR3>
2023-06-01 14:54:16 -0300

    Add project files.

---
+++ helloworldDMA_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("helloworldDMA_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("helloworldDMA_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("034255F5-FEA0-44ED-A3BF-BBA2BFA6E513")]
+[assembly: AssemblyVersion("1.0.0.0")]
